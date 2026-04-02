1430bc4ff3e0a1cbf9a4e0c3b674e1212a4d99cd
coolestmonkey1996-pixel <coolestmonkey1996@gmail.com>
2026-04-03 01:12:34 +0700

    Create paje.js

---
+++ app/paje.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>My First Website</h1>
+      <p>I am building this site from scratch.</p>
+      <button>Learn more</button>
+    </main>
+  );
+}
