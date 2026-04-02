ebdccbf234c557e340383c442284ca5b23ac8f80
coolestmonkey1996-pixel <coolestmonkey1996@gmail.com>
2026-04-03 01:28:51 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,25 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="hero">
+
+      <div className="hero-content">
+        <p className="hero-label">PAIN TATTOO</p>
+
+        <h1>
+          Dark aesthetics.
+          <br />
+          Clean execution.
+        </h1>
+
+        <p className="hero-text">
+          Custom tattoos with strong visual identity and precision.
+        </p>
+
+        <button className="hero-button">
+          Book session
+        </button>
+      </div>
+
     </main>
   );
 }
