58dd2997e8984fcb959433bc37d9a3abd371d2ab
coolestmonkey1996-pixel <coolestmonkey1996@gmail.com>
2026-04-03 01:23:03 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,7 @@
 export default function Home() {
   return (
-    <main className="hero">
-      <div className="hero-content">
-        <p className="hero-label">WELCOME</p>
-        <h1>My First Website</h1>
-        <p className="hero-text">
-          I am learning how to build websites step by step.
-        </p>
-        <button>Learn more</button>
-      </div>
+    <main>
+      <h1>Hello World</h1>
     </main>
   );
 }
-
